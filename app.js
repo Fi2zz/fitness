6a0a88e44dd74649bf7aab4541a79a7f552d879d
Fitz <fitz.wen@edragonconnect.com>
2020-06-28 11:24:52 +0800

    update

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 window.onload =function(){
-    document.getElementById('time').innerText = "Dist at 2020-6-24 22:48:57";
+    document.getElementById('time').innerText = "Dist at 2020-6-28 11:24:52";
 }
